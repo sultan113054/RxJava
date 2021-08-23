01043d09cc19ae40f03e34a337099014f50c6588
sultan.mahmud <sultan.mahmud@bracits.com>
2021-08-23 15:59:53 +0600

    third chapter in progress

---
+++ src/third_chapter/CollectionOperator.java
@@ -0,0 +1,37 @@
+package third_chapter;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CollectionOperator {
+    public static void main(String[] args) {
+       /* Observable.just("1","2","3")
+                .toList()
+                .subscribe(p-> System.out.println(p));*/
+
+  /*      Observable.just("4","3","2")
+                .toSortedList()
+                .subscribe(p-> System.out.println(p));*/
+
+      /*  Observable.just("abc","def")
+                .toMap(p->p.charAt(0))
+                .subscribe(p-> System.out.println(p));
+
+        Observable.just("abc","def")
+                .toMap(p->p.charAt(0),String::length)
+                .subscribe(p-> System.out.println(p));*/
+
+     /*   Observable.just("abc", "def")
+                .toMap(p -> p.charAt(0), String::length, ConcurrentHashMap::new)
+                .subscribe(p -> System.out.println(p));*/
+        /*Observable.just("abc", "def")
+                .toMap( String::length)
+                .subscribe(p -> System.out.println(p));*/
+
+        Observable.just("abc", "def")
+                .toMultimap( String::length)
+                .subscribe(p -> System.out.println(p));
+    }
+}
